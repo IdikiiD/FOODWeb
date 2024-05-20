50d4e4e720dba9dcbac6ffc81f278b0b7a1618a6
S2v2ge <88112165+S2v2ge@users.noreply.github.com>
2024-05-20 13:46:41 -0700

    Refactor Timer

--- js/script.js
+++ js/script.js
@@ -36,13 +36,23 @@ window.addEventListener('DOMContentLoaded', function () {
     });
 
     // Timer
-    const endDate = "2034-12-13";
+    const endDate = "2024-05-20";
 
     function getRemaining(endTime) {
-        const t = Date.parse(endDate) - Date.now(), days = Math.floor(t / (1000 * 60 * 60 * 24)),
-            hours = Math.floor((t / (1000 * 60 * 60)) % 24), minutes = Math.floor((t / 1000 / 60) % 60),
-            seconds = Math.floor((t / 1000) % 60);
-
+        const t = Date.parse(endDate) - Date.now();
+        let days, hours, minutes, seconds;
+            if
+        (t <= 0)
+        {
+            days = hours = minutes = seconds = 0;
+        }
+    else
+        {
+                days = Math.floor(t / (1000 * 60 * 60 * 24)),
+                hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+                minutes = Math.floor((t / 1000 / 60) % 60),
+                seconds = Math.floor((t / 1000) % 60);
+        }
         return {
             total: t, days: days, hours: hours, minutes: minutes, seconds: seconds
         };
@@ -167,7 +177,7 @@ window.addEventListener('DOMContentLoaded', function () {
     const getResourse = async (url, data) => {
         const res = await fetch(url);
 
-        if (!res.ok){
+        if (!res.ok) {
             throw new Error(`Could not fetch ${url}, status${res.status}`);
         }
 
@@ -175,10 +185,10 @@ window.addEventListener('DOMContentLoaded', function () {
     };
 
     getResourse('http://localhost:3000/menu')
-        .then(data=>{
-           data.forEach(({img,altimg,title,descr,price}) => {
-               new MenuTab(img,altimg,title,descr,price, '.menu .container').render();
-           })
+        .then(data => {
+            data.forEach(({img, altimg, title, descr, price}) => {
+                new MenuTab(img, altimg, title, descr, price, '.menu .container').render();
+            })
         });
 
 
@@ -221,8 +231,7 @@ window.addEventListener('DOMContentLoaded', function () {
             const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
 
-
-                postData('http://localhost:3000/requests', json)
+            postData('http://localhost:3000/requests', json)
                 .then(data => {
                     console.log(data);
                     showThanksModal(message.success)
@@ -272,21 +281,21 @@ window.addEventListener('DOMContentLoaded', function () {
     //Slider
 
     let slideIndex = 1;
-    let offset = 0 ;
+    let offset = 0;
     const slides = document.querySelectorAll('.offer__slide'),
-          prev = document.querySelector('.offer__slider-prev'),
-          next = document.querySelector('.offer__slider-next'),
-          total = document.querySelector('#total'),
-          current = document.querySelector('#current'),
-          slidesWrapper = document.querySelector('.offer__slider-wrapper'),
-          slidesField = document.querySelector('.offer__slider__inner'),
-          width = window.getComputedStyle(slidesWrapper).width;
+        prev = document.querySelector('.offer__slider-prev'),
+        next = document.querySelector('.offer__slider-next'),
+        total = document.querySelector('#total'),
+        current = document.querySelector('#current'),
+        slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+        slidesField = document.querySelector('.offer__slider__inner'),
+        width = window.getComputedStyle(slidesWrapper).width;
 
 
-    if (slides.length < 10){
+    if (slides.length < 10) {
         total.textContent = `0${slides.length}`
         current.textContent = `0${slideIndex}`
-    }else{
+    } else {
         total.textContent = slides.length
         current.textContent = slideIndex;
     }
@@ -298,55 +307,55 @@ window.addEventListener('DOMContentLoaded', function () {
 
     slidesWrapper.style.overflow = 'hidden';
 
-    slides.forEach(item =>{
+    slides.forEach(item => {
         item.style.width = width;
     })
 
-    next.addEventListener('click',() => {
+    next.addEventListener('click', () => {
 
-        if (offset === +width.slice(0,width.length - 2) * (slides.length -1)){
+        if (offset === +width.slice(0, width.length - 2) * (slides.length - 1)) {
             offset = 0;
-        }else{
-            offset += +width.slice(0,width.length - 2);
+        } else {
+            offset += +width.slice(0, width.length - 2);
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`
 
-        if(slideIndex === slides.length){
+        if (slideIndex === slides.length) {
             slideIndex = 1;
-        }else{
+        } else {
             slideIndex++;
         }
 
-        if(slides.length < 10){
+        if (slides.length < 10) {
             current.textContent = `0${slideIndex}`;
-        }else{
+        } else {
             current.textContent = slideIndex
         }
     })
 
-    prev.addEventListener('click',() => {
+    prev.addEventListener('click', () => {
 
-        if (offset == 0){
-            offset += +width.slice(0,width.length - 2) * (slides.length -1)
-        }else{
-            offset -= +width.slice(0,width.length - 2);
+        if (offset == 0) {
+            offset += +width.slice(0, width.length - 2) * (slides.length - 1)
+        } else {
+            offset -= +width.slice(0, width.length - 2);
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`
 
-        if(slideIndex === 1){
+        if (slideIndex === 1) {
             slideIndex = slides.length;
-        }else{
+        } else {
             slideIndex--;
         }
 
-        if(slides.length < 10){
+        if (slides.length < 10) {
             current.textContent = `0${slideIndex}`;
-        }else{
+        } else {
             current.textContent = slideIndex
         }
     })
- });
+});
 
 
